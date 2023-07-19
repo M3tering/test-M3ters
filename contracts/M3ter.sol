07ca48c1d3a09013e57a2ca5687cd30ae36681bf
ichristwin <ichristwin@gmail.com>
2023-07-19 13:33:56 +0100

    reverting to using int IDs

--- contracts/M3ter.sol
+++ contracts/M3ter.sol
@@ -1,29 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+import "./XRC721.sol";
+import "./IM3ter.sol";
 
 /// @custom:security-contact info@whynotswitch.com
-contract M3ter is
-    Initializable,
-    UUPSUpgradeable,
-    ERC721Upgradeable,
-    PausableUpgradeable,
-    AccessControlUpgradeable,
-    ERC721BurnableUpgradeable,
-    ERC721EnumerableUpgradeable
-{
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+contract M3ter is XRC721, IM3ter {
+    bytes32 public constant REGISTRAR_ROLE = keccak256("REGISTRAR_ROLE");
 
-    event Register(uint indexed id, uint timestamp, address from);
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    mapping(uint256 => bytes32) REGISTRY;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -33,60 +23,36 @@ contract M3ter is
     function initialize() public initializer {
         __ERC721_init("M3ter", "M3R");
         __ERC721Enumerable_init();
-        __UUPSUpgradeable_init();
-        __ERC721Burnable_init();
+        __ERC721URIStorage_init();
         __AccessControl_init();
-        __Pausable_init();
+        __UUPSUpgradeable_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(REGISTRAR_ROLE, msg.sender);
         _grantRole(UPGRADER_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://m3ter.whynotswitch.com/";
     }
 
-    function _register(uint id) public onlyRole(MINTER_ROLE) {
-        emit Register(id, block.timestamp, msg.sender);
-        _safeMint(msg.sender, id);
+    function _register(
+        uint256 tokenId,
+        bytes32 deviceId
+    ) public onlyRole(REGISTRAR_ROLE) {
+        require(_exists(tokenId), "M3ter: can't register, M3ter doesn't exist");
+        emit Register(tokenId, deviceId, block.timestamp, msg.sender);
+        REGISTRY[tokenId] = deviceId;
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
+    function safeMint() public payable onlyRole(REGISTRAR_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyRole(UPGRADER_ROLE) {}
-
-    // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint id,
-        uint batchSize
-    ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
-        super._beforeTokenTransfer(from, to, id, batchSize);
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(
-            ERC721Upgradeable,
-            ERC721EnumerableUpgradeable,
-            AccessControlUpgradeable
-        )
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function identify(uint256 tokenId) external view returns (bytes32) {
+        require(_exists(tokenId), "M3ter: device doesn't exist");
+        return REGISTRY[tokenId];
     }
 }
---
+++ contracts/XRC721.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+abstract contract XRC721 is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721EnumerableUpgradeable,
+    ERC721URIStorageUpgradeable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
+
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyRole(UPGRADER_ROLE) {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable,
+            ERC721URIStorageUpgradeable,
+            AccessControlUpgradeable
+        )
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
